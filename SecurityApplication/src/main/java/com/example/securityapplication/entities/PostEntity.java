128ca20022b778e66ded6caf8fff2682ddb0f418
sagar bankar <iamsagarbankar@gmail.com>
2026-04-09 15:01:27 +0530

    setUp spring security app

---
+++ SecurityApplication/src/main/java/com/example/securityapplication/entities/PostEntity.java
@@ -0,0 +1,10 @@
+package com.example.securityapplication.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="posts")
+public class PostEntity {
+
+}
